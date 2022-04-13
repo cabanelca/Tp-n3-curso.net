778052ebfb2fbfb4ab9b3f959def39c094adf0ef
Ruben O Cabanellas <rucaba14@gmail.com>
2022-04-13 16:28:12 -0300

    se creo repositorio tp3

---
+++ ejercicio1/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+
+{
+    internal class Persona
+    {
+        private string nombre;
+        private int sueldo;
+        private string sexo;
+        private double peso;
+        private double altura;
+        private string email;
+        public Persona(string nombre, int sueldo, string sexo, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.sueldo = sueldo;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+            CrearEmail();
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public int GetSueldo()
+        {
+            return sueldo;
+        }
+        public string GetSexo()
+        {
+            return sexo;
+        }
+        public double GetPeso()
+        {
+            return peso;
+        }
+        public double GetAltura()
+        {
+            return altura;
+        }
+        private void CrearEmail()
+        {
+            this.email = nombre + "@superempresa.com";
+        }
+
+    }
+}
+
+
---
+++ ejercicio1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string nombre;
+            int sueldo;
+            string sexo;
+            double peso;
+            double altura;
+
+
+
+            Console.WriteLine("INGRESE EL NOMBRE");
+            nombre = Console.ReadLine();
+            Console.WriteLine("INGRESE EL SUELDO");
+            sueldo =int.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE SU GENERO");
+            sexo = Console.ReadLine();
+            Console.WriteLine("INGRESE SU PESO");
+            peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE SU ALTURA");
+            altura = double.Parse(Console.ReadLine());
+
+            Persona persona = new Persona(nombre,sueldo,sexo,peso,altura);
+            Console.WriteLine("");
+            Console.WriteLine("Nombre: " + persona.GetNombre());
+            Console.WriteLine("Email: " + persona.GetEmail());
+            Console.WriteLine("Sueldo: " + persona.GetSueldo());
+            Console.WriteLine("Sexo: " + persona.GetSexo());
+            Console.WriteLine("Peso: " + persona.GetPeso());
+            Console.WriteLine("Altura: " + persona.GetAltura());
+            Console.ReadLine();
+
+        }
+    }
+}
+    
+
